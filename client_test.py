40a35e7ab680f29456e36298b2764b6515303e1e
shivam1903 <shivam234agarwal@gmail.com>
2023-12-25 15:16:54 +0530

    Adding the test files

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -7,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2 ))
     """ ------------ Add the assertion below ------------ """
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
@@ -14,7 +17,35 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2 ))
     """ ------------ Add the assertion below ------------ """
+
+  def test_getRatio_calculateRatio(self):
+    quotes = [
+      {'price_a': 121.68, 'price_b': 119.56},
+      {'price_a': 128.68, 'price_b': 122.44}
+    ]
+    for quote in quotes:
+       self.assertEqual(getRatio(quote['price_a'], quote['price_b']), quote['price_a']/quote['price_b'])
+  
+  def test_getRatio_calculateRatioPriceA0(self):
+    quotes = [
+      {'price_a': 0, 'price_b': 121.68},
+      {'price_a': 0, 'price_b': 125.68}
+    ]
+    for quote in quotes:
+       self.assertEqual(getRatio(quote['price_a'], quote['price_b']), 0)
+
+  def test_getRatio_calculateRatioPriceB0(self):
+    quotes = [
+      {'price_a': 121.68, 'price_b': 0},
+      {'price_a': 125.68, 'price_b': 0}
+    ]
+    for quote in quotes:
+       self.assertIsNone(getRatio(quote['price_a'], quote['price_b']))
+  
+
 
 
   """ ------------ Add more unit tests ------------ """
